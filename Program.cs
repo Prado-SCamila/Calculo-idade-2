624722ae4c42f116906786129503135a149c9caa
Prado-SCamila <prado.camila.910423@gmail.com>
2020-11-13 10:57:49 -0300

    Projeto de console finalizado

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculo_idade_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iSemana, anoNasc , i;
+            int anoAtual = 2020;
+
+            Console.WriteLine("Digite seu ano de nascimento");
+            anoNasc = int.Parse(Console.ReadLine());
+
+            i = anoAtual - anoNasc;
+
+            Console.WriteLine("Sua idade é: "+i);
+
+            iSemana = i*48;
+            Console.WriteLine("Sua idade em semanas é: "+iSemana);
+
+    
+        }
+    }
+}
